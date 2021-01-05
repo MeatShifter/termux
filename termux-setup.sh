f7967ffd2970bb0d4fe085fffbda6dc0e89b1060
MeatShifter <76061319+MeatShifter@users.noreply.github.com>
2021-01-05 16:17:03 -0500

    Create termux-setup.sh
    Rough draft gather:
    commands
    programs
    configs

---
+++ termux-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+echo='***************************************&'
+echo='*----- MagicPuddle Termux Config ------*'
+echo='*----- Ver. 1.0.0 By: MeatShifter -----*'
+echo='****************************************'
+# Upgrade PKG's
+pkg upgrade
+echo='Installing Packages'
+pkg add vim tmux git coreutils termux-api termux-services termux-exec termux-tools grep tree ncurses-utils openssh gpg nginx mariadb python cmake nodejs ruby golang
+# storage setup
+termux-setup-storage
+# configs from GitHub
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
